bc974023fab0fd1a93afb45a0fcf7511c1e9e00d
vlad727 <41288192+vlad727@users.noreply.github.com>
2022-02-17 19:35:48 +0300

    metod with strings

--- f-string-example.py
+++ f-string-example.py
@@ -5,3 +5,11 @@ balance = 32.65
 text = f""" Дорогой {name} {mid_name}, баланс Вашего лицевого счета составляет {balance}  """
 
 print(text)
+
+name = 'Семен'
+mid_name = 'Семенович'
+balance = 32.65
+
+text = f""" Дорогой {name.upper()} {mid_name.lower()}, баланс Вашего лицевого счета составляет {balance}  """
+
+print(text)
