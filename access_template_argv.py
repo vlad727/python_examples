b003e755424163ab5ea90ba1242d5474d91c684d
vlad727 <kyz727@mail.ru>
2022-02-25 17:27:28 +0300

    added files about args and python net

---
+++ access_template_argv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+interface = argv[1]
+vlan = argv[2]
+
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
